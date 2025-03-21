ac5eb13bf97874c36bd1b200029462d7df113da2
Gunjan Datta <gudatta@microsoft.com>
2025-03-21 09:38:38 -0400

    Updated for next release.

--- src/strings.ts
+++ src/strings.ts
@@ -23,6 +23,6 @@ const Strings = {
     ProjectDescription: "Application for adminitrators to make requests for changes on their site collections.",
     SourceUrl: ContextInfo.webServerRelativeUrl,
     TimeFormat: "YYYY-MMM-DD HH:mm:ss",
-    Version: "0.2"
+    Version: "0.3"
 };
 export default Strings;
