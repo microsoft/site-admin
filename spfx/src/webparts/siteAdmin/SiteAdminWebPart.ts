4ab4cb20c1de476e1113ab4107a02af0126152a0
Gunjan Datta <gudatta@microsoft.com>
2026-03-16 14:06:51 -0400

    Updated the max requests to be 5 not 10.

--- spfx/src/webparts/siteAdmin/SiteAdminWebPart.ts
+++ spfx/src/webparts/siteAdmin/SiteAdminWebPart.ts
@@ -522,12 +522,7 @@ export default class SiteAdminWebPart extends BaseClientSideWebPart<ISiteAdminWe
                     { key: 2, text: "2" },
                     { key: 3, text: "3" },
                     { key: 4, text: "4" },
-                    { key: 5, text: "5" },
-                    { key: 6, text: "6" },
-                    { key: 7, text: "7" },
-                    { key: 8, text: "8" },
-                    { key: 9, text: "9" },
-                    { key: 10, text: "10" }
+                    { key: 5, text: "5" }
                   ]
                 }),
                 PropertyPaneToggle("SiteAttestation", {
