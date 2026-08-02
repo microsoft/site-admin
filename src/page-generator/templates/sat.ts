4804a6b7e9a5b7bfdf10b2fce990b30052a0ac5b
Gunjan Datta <gudatta@microsoft.com>
2026-08-02 19:25:58 -0400

    Fixed reference.

--- src/page-generator/templates/sat.ts
+++ src/page-generator/templates/sat.ts
@@ -89,4 +89,4 @@ const CanvasContent = `<div>
 // Export the page
 export const SAT = new PageTemplate(CanvasContent, "8a25e50f-a830-4e23-bfd3-38aaa20b57ba",
     "caf33edc-08f1-46f1-be6f-3946a595ebc3", "416cc6dd-2088-4197-91e2-3f6f7b4c6219",
-    "/sites/Dev/SiteAssets/SitePages/PageGenerator", {});
+    "/sites/Demo/SiteAssets/SitePages/PageGenerator", {});
