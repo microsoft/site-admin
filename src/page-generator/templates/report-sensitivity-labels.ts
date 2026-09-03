9bb9bda37912976bbfe27083891c9be539ca8fb1
Gunjan Datta <gudatta@microsoft.com>
2026-09-03 16:59:47 -0400

    Added new report pages - pending images to be added.

---
+++ src/page-generator/templates/report-sensitivity-labels.ts
@@ -0,0 +1,14 @@
+import { PageTemplate } from "../template";
+
+// The html content for the webpart
+const CanvasContent = ``;
+
+// Export the page
+// Input Params: content, siteId, webId, listId, folderUrl, imageMapper
+export const AppPage = new PageTemplate(CanvasContent, "",
+    "", "",
+    "/sites/Demo/SiteAssets/SitePages/SATAuditReportsSensitivityLabels.aspx", {
+    "": "",
+    "": "",
+    "": ""
+});
